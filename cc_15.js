6b545a02b8d3261ed663c5cde01908fee049c425
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-03-16 22:17:46 -0400

    Task 2 - Adding Risk Items

--- cc_15.js
+++ cc_15.js
@@ -1,3 +1,21 @@
 // Task 1 Creating the Basic Structure
 const riskDashboard = document.getElementById("riskDashboard");
 console.log("risk dashboard loaded") ; 
+
+
+//Task 2 Adding Risk Items Dynamically 
+function addRiskItem (riskName, riskLevel, department )
+{const riskCard = document.createElement("div");
+    riskCard.classList.add ="riskcard" ;
+    
+    riskCard.innerHTML = `
+    <p>${riskName}</p>
+    <p> Risk Level: ${riskLevel}</p>
+    <p> Department: ${department}</p>
+    `
+    riskDashboard.appendChild(riskCard);
+    
+    
+}
+addRiskItem("Data Breach", "High", "IT");
+addRiskItem("Supply Chain Disruption", "Medium", "Operations");
